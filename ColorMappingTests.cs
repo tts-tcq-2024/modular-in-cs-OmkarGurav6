3cd0e07ff0aec5f0e7e41ca038a8ec111e608749
Omkar Gurav <64888669+OmkarGurav6@users.noreply.github.com>
2024-07-23 21:09:07 +0530

    Update ColorMappingTests.cs

--- ColorMappingTests.cs
+++ ColorMappingTests.cs
@@ -4,60 +4,6 @@ using System.Drawing;
 
 namespace TelCo.ColorCoder
 {
-   
-    class Program
-    {
-        private static ColorPair GetColorFromPairNumber(int pairNumber)
-        {
-            int minorSize = colorMapMinor.Length;
-            int majorSize = colorMapMajor.Length;
-            if (pairNumber < 1 || pairNumber > minorSize * majorSize)
-            {
-                throw new ArgumentOutOfRangeException(
-                    string.Format("Argument PairNumber:{0} is outside the allowed range", pairNumber));
-            }
-            
-            int zeroBasedPairNumber = pairNumber - 1;
-            int majorIndex = zeroBasedPairNumber / minorSize;
-            int minorIndex = zeroBasedPairNumber % minorSize;
-
-            ColorPair pair = new ColorPair() { majorColor = colorMapMajor[majorIndex],
-                minorColor = colorMapMinor[minorIndex] };
-            
-            return pair;
-        }
-       
-        private static int GetPairNumberFromColor(ColorPair pair)
-        {
-            int majorIndex = -1;
-            for (int i = 0; i < colorMapMajor.Length; i++)
-            {
-                if (colorMapMajor[i] == pair.majorColor)
-                {
-                    majorIndex = i;
-                    break;
-                }
-            }
-
-            int minorIndex = -1;
-            for (int i = 0; i < colorMapMinor.Length; i++)
-            {
-                if (colorMapMinor[i] == pair.minorColor)
-                {
-                    minorIndex = i;
-                    break;
-                }
-            }
-            
-            if (majorIndex == -1 || minorIndex == -1)
-            {
-                throw new ArgumentException(
-                    string.Format("Unknown Colors: {0}", pair.ToString()));
-            }
-
-            return (majorIndex * colorMapMinor.Length) + (minorIndex + 1);
-        }
-       
      public class ColorMappingTests
     {
         public static void RunTests()
@@ -68,6 +14,7 @@ namespace TelCo.ColorCoder
             ValidatePairNumberMapping(Color.Yellow, Color.Green, 18);
             ValidatePairNumberMapping(Color.Red, Color.Blue, 6);
         }
+        
         private static void ValidateColorMapping(int pairNumber, Color expectedMajor, Color expectedMinor)
         {
             ColorPair colorPair = ColorMap.GetColorFromPairNumber(pairNumber);
